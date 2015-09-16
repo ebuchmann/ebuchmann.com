f8c16c62ca0a85fd1d43d526c1e93b59d13e566e
Eric Buchmann <eric.buchmann@popart.com>
2015-09-15 18:35:03 -0700

    added some analytics tracking events

--- _js/analytics.js
+++ _js/analytics.js
@@ -5,3 +5,34 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-40066370-7', 'auto');
 ga('send', 'pageview');
+
+(function() {
+  var originals = document.getElementsByClassName('ga-original');
+  var prints = document.getElementsByClassName('ga-print');
+
+  // track clicks to original paintings on Etsy
+  for( var i = 0; i < originals.length; i++ ) {
+    var original = originals[i];
+
+    original.onclick = function(e) {
+      e.preventDefault();
+
+      var title = this.getAttribute('title');
+
+      ga('send', 'event', 'original', 'click', title)
+    };
+  };
+
+  // track clicks to prints on Etsy
+  for( var i = 0; i < prints.length; i++ ) {
+    var print = prints[i];
+
+    print.onclick = function(e) {
+      e.preventDefault();
+
+      var title = this.getAttribute('title');
+
+      ga('send', 'event', 'print', 'click', title)
+    };
+  };
+})();
